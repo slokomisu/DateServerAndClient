5fda2bbaa2e8e5209215008bac849efbd504e54a
Noah Guillory <noah@noahguillory.me>
2017-02-11 23:10:30 -0600

    Initial commit

---
+++ DateClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DateClient {
+    internal class Program {
+        public static void StartClient() {
+            // buffer for incoming data
+            byte[] bytes = new byte[1024];
+
+            // connect to remote device
+            try {
+                IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 8080);
+
+                // create socket
+                Socket clientSocket = new Socket(AddressFamily.InterNetwork,
+                    SocketType.Stream, ProtocolType.Tcp );
+
+                try {
+                    clientSocket.Connect(remoteEP);
+                    Console.WriteLine("Socket connected to {0}",
+                        clientSocket.RemoteEndPoint.ToString());
+                    int bytesRec = clientSocket.Receive(bytes);
+                    Console.WriteLine("Current time: {0}",
+                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                } catch (ArgumentNullException ane) {
+                    Console.WriteLine("ArgumentNullException : {0}",ane.ToString());
+                } catch (SocketException se) {
+                    Console.WriteLine("SocketException : {0}",se.ToString());
+                } catch (Exception e) {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            } catch (Exception e) {
+                Console.WriteLine( e.ToString());
+            }
+
+        }
+
+        public static void Main(string[] args) {
+            StartClient();
+        }
+    }
+}
---
+++ DateServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DateServer {
+    internal class Program {
+        public static void StartListening() {
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localendPoint = new IPEndPoint(ipAddress, 8080);
+
+            // create socket
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream,
+                ProtocolType.Tcp);
+            // bind to local endpoint and listen for connections
+            try {
+                listener.Bind(localendPoint);
+                listener.Listen(10);
+
+                while (true) {
+                    Console.WriteLine("Waiting for a connection");
+                    Socket handler = listener.Accept();
+
+                    // get current date
+                    var now = DateTime.Now.ToString();
+                    // encode data for client
+                    byte[] date = Encoding.ASCII.GetBytes(now);
+                    Console.WriteLine("Sent current time to {0}", handler.RemoteEndPoint.ToString());
+
+                    handler.Send(date);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+        }
+
+        public static void Main(string[] args) {
+            StartListening();
+        }
+    }
+}
